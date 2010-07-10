4a882d3e0ea1867ae524bfab685539849359f164
Moritz Lenz <moritz@faui2k3.org>
2010-07-10 14:57:41 +0200

    Component::Graph stub

---
+++ lib/Tufte/Component/Graph.pm
@@ -0,0 +1,13 @@
+use Tufte::Component::Base;
+
+class Tufte::Component::Graph is Tufte::Component::Base {
+    our $STACKED_OPACITY = 0.85;
+
+    multi method draw($svg, %bounds, %options) {
+        # TODO: implement
+
+    }
+
+}
+
+# vim: ft=perl6
