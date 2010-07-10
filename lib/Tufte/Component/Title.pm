d0f50baabf7f79e9e601726fb7d145635ada6629
Moritz Lenz <moritz@faui2k3.org>
2010-07-10 15:07:04 +0200

    initial Component::Title

---
+++ lib/Tufte/Component/Title.pm
@@ -0,0 +1,22 @@
+use Tufte::Component::Base;
+
+class Tufte::Component::Title is Tufte::Component::Base {
+    multi method draw($svg, %bounds, %options) {
+        if %options.exists<title> {
+            $svg.drawings.push: :text[
+                :class<title>,
+                :x(%bounds<width> / 2),
+                :y(%bounds<height>),
+                # TODO: promote these to :style(something)
+                :font-size($.relative(100)),
+                :font-family(%options<theme>.font-family),
+                :fill(%options<theme>.marker),
+                :stroke("none; stroke-width: 0"),
+                :text-anchor(%options<text_anchor> // 'middle'),
+                ~%options<title>,
+            ],
+        }
+    }
+}
+
+# vim: ft=perl6
