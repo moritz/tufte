6525b147fa241e738ce296b40a5e4f77e5e810d2
Hongwen Qiu <qiuhongwen@gmail.com>
2010-07-14 16:44:47 +0800

    [Tufte/Component/Base.pm] import all of the methods for Scrufy/Component/Base.pm.

--- lib/Tufte/Component/Base.pm
+++ lib/Tufte/Component/Base.pm
@@ -1,10 +1,37 @@
 class Tufte::Component::Base;
 
 has $.id;
-has @.position = (0, 0);
-has @.size = (100, 100);
-has %.options;
-has $.visible = True;
+has @.position is rw = (0, 0);
+has @.size is rw = (100, 100);
+has %.options is rw;
+has $.visible is rw = True;
+has $!render_height;
 
+multi method render($svg, %bounds, %options) {
+    return unless $.visible;
+    %options = %options, %.options;
+
+    return $.process($svg, %options) unless %bounds;
+
+    $!render_height = %bounds<height>;
+
+    $svg.drawings.push: g => [
+        id => $.id,
+        transform => "translate(%bounds.delete('x'), %bounds.delete('y'))"
+    ];
+    $.draw($svg, %bounds, %options);
+}
+
+multi method draw($svg, %bounds, %options) {
+    # Override this if visual component
+}
+
+multi method process($svg, %options) {
+    # Override this NOT a visual component
+}
+
+multi method relative($pct) {
+    $!render_height * ($pct/100)
+}
 
 # vim: ft=perl6 sw=4 ts=4 expandtab
