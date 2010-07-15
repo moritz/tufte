1b63a9171a35be8a6e75836adc353143a840c591
Hongwen Qiu <qiuhongwen@gmail.com>
2010-07-15 14:04:50 +0800

    add Tufte::Component::Grid

---
+++ lib/Tufte/Component/Grid.pm
@@ -0,0 +1,20 @@
+use Tufte::Component::Base;
+
+class Tufte::Component::Grid is Tufte::Component::Base {
+    has $.markers is rw;
+
+    multi method draw($svg, %bounds, %options) {
+	$!markers = %options<markers> // $!markers // 5;
+
+	for 0..$!markers -> $idx {
+	    my $marker = ((1 / ($!markers - 1)) * $idx) * %bounds<height>;
+	    $svg.drawings.push: :line[
+	        :x1<0>, y1 => $marker,
+		x2 => %bounds<width>, y2 => $marker,
+		style => "stroke: %options<theme>.marker(); stroke-width: 2;"
+	    ];
+	}
+    }
+}
+
+# vim: sw=4 ts=4 ft=perl6 expandtab
