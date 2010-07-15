85000396fff15c5901c9e49e88d3fde5bf470e83
Hongwen Qiu <qiuhongwen@gmail.com>
2010-07-15 17:19:12 +0800

    Add Tufte::Component::ValueMarkers.

---
+++ lib/Tufte/Component/ValueMarkers.pm
@@ -0,0 +1,35 @@
+use Tufte::Component::Base;
+
+class Tufte::Component::ValueMarkers is Tufte::Component::Base;
+
+has $.markers is rw;
+
+multi method draw($svg, %bounds, %options) {
+    my $markers = %options<markers> // $!markers // 5;
+    my @all_values;
+
+    for 0..$markers -> $idx {
+	@all_values.push: (%options<max_value> - %options<min_value>) * ((1 / ($markers - 1)) * $idx) + %options<min_value>;
+    }
+
+    for 0..$markers -> $idx {
+	my $marker_unit = (1 / ($markers - 1)) * $idx;
+	my $marker = $marker_unit * %bounds<height>;
+	my $marker_value = (%options<max_value> - %options<min_value>) * $marker_unit + %options<min_value>;
+	if %options<value_formatter> {
+	    %options<all_values> = @all_values;
+	    $marker_value    = %options<value_formatter>.route_format($marker_value, $idx, %options);
+	}
+
+	$svg.drawings.push: :text[
+	    :x(%bounds<width>), :y(%bounds<height> - $marker),
+	    :font-size($.relative(8)),
+	    :font-family(%options<theme>.font_family),
+	    :fill(~(%options.delete<marker_color_override> || %options<theme>.marker || 'white')),
+	    :text-anchor<end>,
+	    ~$marker_value
+	];
+    }
+}
+
+# vim: sw=4 ts=4 ft=perl6 expandtab
