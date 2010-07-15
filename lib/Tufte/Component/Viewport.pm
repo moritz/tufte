c99b871270a6c7035ceaac5393ff5b1e224dc121
Hongwen Qiu <qiuhongwen@gmail.com>
2010-07-15 21:41:27 +0800

    Add Tufte::Component::Viewport.

---
+++ lib/Tufte/Component/Viewport.pm
@@ -0,0 +1,24 @@
+use Tufte::Component::Base;
+use Tufte::Helper::Canvas;
+
+class Tufte::Component::Viewport is Tufte::Component::Base;
+
+has @!components;
+
+multi method draw(%bounds, %options) {
+    for @!components -> $component {
+        $!svg.drawings.push: $component.render($.bounds_for([%bounds<width>, %bounds<height>], $component.position, $component.size), %options);
+    }
+    $!svg.drawings = :g[
+        $.options_for,
+        $!svg.drawings
+    ];
+}
+
+multi method options_for() {
+    my %options;
+    %options<transform> ~= "$_($!options{$_})" for <skewX skewY>;
+    %options;
+}
+
+# vim: sw=4 ts=4 ft=perl6 expandtab
